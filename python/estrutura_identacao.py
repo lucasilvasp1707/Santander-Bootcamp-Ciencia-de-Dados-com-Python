39ac5d734cefe5fbeec768accb20051d78116f4a
Lucas Silva <lucasilvasp1707@gmail.com>
2025-06-27 02:08:53 -0300

    feat: add lucasilvasp1707

--- python/estrutura_identacao.py
+++ python/estrutura_identacao.py
@@ -5,6 +5,7 @@ def sacar(valor: int):
         saldo -=valor
         print("seu saldo",saldo)
 
+
 sacar(100)        
 
 
@@ -13,3 +14,21 @@ def exibanome(nome:str):
     print(nome2)
 
 exibanome("lucas")
+
+def tirar_cnh(idade:int):
+    if idade >= 18:
+        print("pode tirar cnh ja é maior de idade")
+
+    elif idade >=16 and idade <18:
+        print('pode tirar com autorização dos pais')    
+
+    else:
+        print("não pode tirar a cnh")
+    status ="sucesso " if idade >=18 else "nao pode tirar a cnh no momento"    
+    print( status ,"resultado do if ternario" )
+
+tirar_cnh(18)         
+
+
+#if ternario
+
