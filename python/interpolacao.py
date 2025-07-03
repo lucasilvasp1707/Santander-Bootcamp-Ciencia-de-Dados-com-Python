80d78be309bc57b575ca88c94f4fca6d01fa2368
Lucas Silva <lucasilvasp1707@gmail.com>
2025-07-03 01:36:02 -0300

    interpolação

---
+++ python/interpolacao.py
@@ -0,0 +1,7 @@
+nome = "lucas"
+idade = 23
+profissao = "dev"
+
+print("ola, me chamo %s, tenho %d e estou atuando como %s" %(nome,idade,profissao)) #interpolaçao%
+print("ola, me chamo {}, tenho {} e estou atuando como {}" .format(nome,idade,profissao))#interpolção com {}
+print(f"ola, me chamo {nome}, tenho {idade} e estou atuando como {profissao}")#interpolação f string
